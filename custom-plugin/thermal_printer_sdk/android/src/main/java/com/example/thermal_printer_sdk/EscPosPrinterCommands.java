52ea9d0ff9d2feb64d1ccc4858114b5329982243
Naveen <sjnaveenkumar@gmail.com>
2024-07-25 21:28:23 +0530

    qrcode issue fix

--- custom-plugin/thermal_printer_sdk/android/src/main/java/com/example/thermal_printer_sdk/EscPosPrinterCommands.java
+++ custom-plugin/thermal_printer_sdk/android/src/main/java/com/example/thermal_printer_sdk/EscPosPrinterCommands.java
@@ -7,12 +7,17 @@ import com.example.thermal_printer_sdk.barcode.Barcode;
 import com.example.thermal_printer_sdk.exceptions.EscPosBarcodeException;
 import com.example.thermal_printer_sdk.exceptions.EscPosConnectionException;
 import com.example.thermal_printer_sdk.exceptions.EscPosEncodingException;
-//import com.google.zxing.EncodeHintType;
-//import com.google.zxing.WriterException;
-//import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-//import com.google.zxing.qrcode.encoder.ByteMatrix;
-//import com.google.zxing.qrcode.encoder.Encoder;
-//import com.google.zxing.qrcode.encoder.QRCode;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.google.zxing.qrcode.encoder.ByteMatrix;
+import com.google.zxing.qrcode.encoder.Encoder;
+import com.google.zxing.qrcode.encoder.QRCode;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.google.zxing.qrcode.encoder.ByteMatrix;
+import com.google.zxing.qrcode.encoder.Encoder;
+import com.google.zxing.qrcode.encoder.QRCode;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
@@ -223,65 +228,65 @@ public class EscPosPrinterCommands {
      */
     public static byte[] QRCodeDataToBytes(String data, int size) throws EscPosBarcodeException {
 
-//        ByteMatrix byteMatrix = null;
-//
-//        try {
-//            EnumMap<EncodeHintType, Object> hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
-//            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-//
-//            QRCode code = Encoder.encode(data, ErrorCorrectionLevel.L, hints);
-//            byteMatrix = code.getMatrix();
-//
-//        } catch (WriterException e) {
-//            e.printStackTrace();
-//            throw new EscPosBarcodeException("Unable to encode QR code");
-//        }
-//
-//        if (byteMatrix == null) {
-//            return EscPosPrinterCommands.initGSv0Command(0, 0);
-//        }
-//
-//        int
-//            width = byteMatrix.getWidth(),
-//            height = byteMatrix.getHeight(),
-//            coefficient = Math.round((float) size / (float) width),
-//            imageWidth = width * coefficient,
-//            imageHeight = height * coefficient,
-//            bytesByLine = (int) Math.ceil(((float) imageWidth) / 8f),
-//            i = 8;
-//
-//        if (coefficient < 1) {
-//            return EscPosPrinterCommands.initGSv0Command(0, 0);
-//        }
-//
-//        byte[] imageBytes = EscPosPrinterCommands.initGSv0Command(bytesByLine, imageHeight);
-//
-//        for (int y = 0; y < height; y++) {
-//            byte[] lineBytes = new byte[bytesByLine];
-//            int x = -1, multipleX = coefficient;
-//            boolean isBlack = false;
-//            for (int j = 0; j < bytesByLine; j++) {
-//                int b = 0;
-//                for (int k = 0; k < 8; k++) {
-//                    if (multipleX == coefficient) {
-//                        isBlack = ++x < width && byteMatrix.get(x, y) == 1;
-//                        multipleX = 0;
-//                    }
-//                    if (isBlack) {
-//                        b |= 1 << (7 - k);
-//                    }
-//                    ++multipleX;
-//                }
-//                lineBytes[j] = (byte) b;
-//            }
-//
-//            for (int multipleY = 0; multipleY < coefficient; ++multipleY) {
-//                System.arraycopy(lineBytes, 0, imageBytes, i, lineBytes.length);
-//                i += lineBytes.length;
-//            }
-//        }
+        ByteMatrix byteMatrix = null;
 
-        return null;
+        try {
+            EnumMap<EncodeHintType, Object> hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+            QRCode code = Encoder.encode(data, ErrorCorrectionLevel.L, hints);
+            byteMatrix = code.getMatrix();
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+            throw new EscPosBarcodeException("Unable to encode QR code");
+        }
+
+        if (byteMatrix == null) {
+            return EscPosPrinterCommands.initGSv0Command(0, 0);
+        }
+
+        int
+            width = byteMatrix.getWidth(),
+            height = byteMatrix.getHeight(),
+            coefficient = Math.round((float) size / (float) width),
+            imageWidth = width * coefficient,
+            imageHeight = height * coefficient,
+            bytesByLine = (int) Math.ceil(((float) imageWidth) / 8f),
+            i = 8;
+
+        if (coefficient < 1) {
+            return EscPosPrinterCommands.initGSv0Command(0, 0);
+        }
+
+        byte[] imageBytes = EscPosPrinterCommands.initGSv0Command(bytesByLine, imageHeight);
+
+        for (int y = 0; y < height; y++) {
+            byte[] lineBytes = new byte[bytesByLine];
+            int x = -1, multipleX = coefficient;
+            boolean isBlack = false;
+            for (int j = 0; j < bytesByLine; j++) {
+                int b = 0;
+                for (int k = 0; k < 8; k++) {
+                    if (multipleX == coefficient) {
+                        isBlack = ++x < width && byteMatrix.get(x, y) == 1;
+                        multipleX = 0;
+                    }
+                    if (isBlack) {
+                        b |= 1 << (7 - k);
+                    }
+                    ++multipleX;
+                }
+                lineBytes[j] = (byte) b;
+            }
+
+            for (int multipleY = 0; multipleY < coefficient; ++multipleY) {
+                System.arraycopy(lineBytes, 0, imageBytes, i, lineBytes.length);
+                i += lineBytes.length;
+            }
+        }
+
+        return imageBytes;
     }
 
     /**
